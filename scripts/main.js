ed7a13466c7e0568d5ce0ec441d4581ea7d9a16d
bribrah <cheesemas46@gmail.com>
2018-10-12 17:05:40 -0700

    added many styling changes notable the nav bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-alert("This website is still under Construction! Please come back soon to view it's progress or feel free to read my first blog post!");
+alert("This website is still under Construction! Please come back soon to view it's progress and feel free to read my first blog post! :)");
